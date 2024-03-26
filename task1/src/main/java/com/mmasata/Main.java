c2c1903a05bcd55d62bce14da95716ef5921ac4f
masatma1 <masatma1@fel.cvut.cz>
2024-03-26 20:28:41 +0100

    Task1: Finalize REST API and add swagger and insomnia files.

--- task1/src/main/java/com/mmasata/Main.java
+++ task1/src/main/java/com/mmasata/Main.java
@@ -3,6 +3,7 @@ package com.mmasata;
 
 import com.mmasata.utils.LinkedList;
 import com.mmasata.utils.List;
+import spark.Response;
 import spark.Route;
 
 import static spark.Spark.get;
@@ -10,7 +11,8 @@ import static spark.Spark.put;
 
 public class Main {
 
-    private static final String MESSAGE_TEMPLATE = "{\"message\" : \"%s\"}";
+    private static final String ERROR_MESSAGE_TEMPLATE = "{\"error\" : \"%s\"}";
+    private static final String OK_MESSAGE_TEMPLATE = "{\"message\" : \"%s\"}";
     private static final String APPLICATION_JSON = "application/json";
 
     private static final List linkedList = new LinkedList();
@@ -25,38 +27,55 @@ public class Main {
     private static Route showList() {
         return (req, res) -> {
             res.type(APPLICATION_JSON);
-            return MESSAGE_TEMPLATE.formatted("LinkedList - %s".formatted(linkedList.toString()));
+            return OK_MESSAGE_TEMPLATE.formatted("LinkedList - %s".formatted(linkedList.toString()));
         };
     }
 
     private static Route insertAfter() {
         return (req, res) -> {
+            res.type(APPLICATION_JSON);
             var o = req.params("o");
             var after = req.params("after");
-            linkedList.insertAfter(o, after);
 
-            res.type(APPLICATION_JSON);
-            return MESSAGE_TEMPLATE.formatted("Value %s was inserted after value %s".formatted(o, after));
+            try {
+                linkedList.insertAfter(o, after);
+                return OK_MESSAGE_TEMPLATE.formatted("Value %s was inserted after value %s".formatted(o, after));
+            } catch (RuntimeException ex) {
+                return returnError(res, ex);
+            }
         };
     }
 
     private static Route pop() {
         return (req, res) -> {
-            var poppedValue = linkedList.pop();
-
             res.type(APPLICATION_JSON);
-            return MESSAGE_TEMPLATE.formatted("Value %s was popped from the LinkedList".formatted(poppedValue));
+
+            try {
+                var poppedValue = linkedList.pop();
+                return OK_MESSAGE_TEMPLATE.formatted("Value %s was popped from the LinkedList".formatted(poppedValue));
+            } catch (RuntimeException ex) {
+                return returnError(res, ex);
+            }
         };
     }
 
     private static Route push() {
         return (req, res) -> {
-            var valueToPush = req.params("value");
-            linkedList.push(valueToPush);
-
             res.type(APPLICATION_JSON);
-            return MESSAGE_TEMPLATE.formatted("Value %s was pushed to the LinkedList".formatted(valueToPush));
+            var valueToPush = req.params("value");
+
+            try {
+                linkedList.push(valueToPush);
+                return OK_MESSAGE_TEMPLATE.formatted("Value %s was pushed to the LinkedList".formatted(valueToPush));
+            } catch (RuntimeException ex) {
+                return returnError(res, ex);
+            }
         };
+    }
+
+    private static String returnError(Response res, RuntimeException ex) {
+        res.status(400);
+        return ERROR_MESSAGE_TEMPLATE.formatted(ex.getMessage());
     }
 
 }
